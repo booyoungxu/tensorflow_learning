32b4392cd5a0da1b1327a917f4680c1afc60447c
booyoungxu <booyoungxu@yeah.net>
2017-07-08 17:15:00 +0800

    implement tensorboard visualize

--- layer_learning.py
+++ layer_learning.py
@@ -4,21 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    layer_name = 'layer_%s' % n_layer
     with tf.name_scope('layer'):
         with tf.name_scope('Weights'):
-            Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-            tf.summary.histogram('Weights', Weights)
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='ww')
+            tf.summary.histogram(layer_name+'/Weights', Weights)
         with tf.name_scope('bias'):
-            bias = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-            tf.summary.histogram('bias', bias)
-        Wx_plus_b = tf.matmul(inputs, Weights) + bias
+            bias = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='bb')
+            tf.summary.histogram(layer_name+'/bias', bias)
+        with tf.name_scope('wx_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + bias
         if activation_function is None:
             output = Wx_plus_b
         else:
             output = activation_function(Wx_plus_b)
-        tf.summary.histogram('outputs', output)
+        tf.summary.histogram(layer_name+'/outputs', output)
         return output
+
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, [300, 1])
 y_data = np.square(x_data) - 0.5 + noise
@@ -27,12 +30,14 @@ with tf.name_scope('inputs'):
     xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-prediction = add_layer(l1, 10, 1, activation_function=None)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
     tf.summary.scalar('loss', loss)
-train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+with tf.name_scope('train'):
+    train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
 
@@ -42,15 +47,14 @@ ax.scatter(x_data, y_data)
 plt.ion() # lianxu
 plt.show()
 with tf.Session() as sess:
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('./', sess.graph) # tensorboard --logdir='./'
+    merged = tf.summary.merge_all()  # 合并所有summary
+    writer = tf.summary.FileWriter('log', sess.graph) # 在tensorflow_learning目录下运行tensorboard --logdir=log 必须将框架图先写入文件
     sess.run(init)
     for i in range(1000):
         sess.run(train, feed_dict={xs: x_data, ys: y_data})
         if i % 50 == 0:
             result = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
             writer.add_summary(result, i)
-            # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
             try:
                 ax.lines.remove(lines[0])
             except Exception:
